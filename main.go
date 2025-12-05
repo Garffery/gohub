cc5c09a0ca8121a637cf5f79a201db67a5f3a457
zjf <13226133323@163.com>
2025-12-05 20:06:35 +0800

    初始化路由

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"gohub/bootstrap"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello, 世界!")
+	// new 一个 Gin Engine 实例
+	router := gin.New()
+
+	// 初始化路由绑定
+	bootstrap.SetupRoute(router)
+
+	// 运行服务
+	// 运行服务
+	err := router.Run(":8080")
+	if err != nil {
+		// 错误处理，端口被占用了或者其他错误
+		fmt.Println(err.Error())
+	}
+
 }
