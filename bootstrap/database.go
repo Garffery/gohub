fc08076974d62d8344c9f44ee3d9670301aebcd4
zjf <13226133323@163.com>
2025-12-07 21:28:59 +0800

    数据库初始化

--- bootstrap/database.go
+++ bootstrap/database.go
@@ -3,12 +3,12 @@ package bootstrap
 import (
 	"errors"
 	"fmt"
+	"gohub/app/models/user"
 	"gohub/pkg/config"
 	"gohub/pkg/database"
 	"time"
 
 	"gorm.io/driver/mysql"
-	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 )
@@ -31,10 +31,10 @@ func SetupDB() {
 		dbConfig = mysql.New(mysql.Config{
 			DSN: dsn,
 		})
-	case "sqlite":
-		// 初始化 sqlite
-		database := config.Get("database.sqlite.database")
-		dbConfig = sqlite.Open(database)
+	// case "sqlite":
+	// 	// 初始化 sqlite
+	// 	database := config.Get("database.sqlite.database")
+	// 	dbConfig = sqlite.Open(database)
 	default:
 		panic(errors.New("database connection not supported"))
 	}
@@ -48,4 +48,6 @@ func SetupDB() {
 	database.SQLDB.SetMaxIdleConns(config.GetInt("database.mysql.max_idle_connections"))
 	// 设置每个链接的过期时间
 	database.SQLDB.SetConnMaxLifetime(time.Duration(config.GetInt("database.mysql.max_life_seconds")) * time.Second)
+
+	database.DB.AutoMigrate(&user.User{})
 }
--- main.go
+++ main.go
@@ -20,13 +20,17 @@ func main() {
 	var env string
 	flag.StringVar(&env, "env", "", "加载 .env 文件，如 --env=testing 加载的是 .env.testing 文件")
 	flag.Parse()
+	fmt.Println("配置加载开始")
 	config.InitConfig(env)
+	fmt.Println("配置加载完成")
 
 	// new 一个 Gin Engine 实例
 	router := gin.New()
 
 	// 初始化 DB
+	fmt.Println("数据库初始化开始")
 	bootstrap.SetupDB()
+	fmt.Println("数据库初始化完成")
 
 	// 初始化路由绑定
 	bootstrap.SetupRoute(router)
